301107d17bfc7b2c7c36b32f7d9ea5b15f47b029
siewilliam01 <siewilliam01@gmail.com>
2025-03-17 10:22:27 -0700

    Update makey_bot_iroman.py

--- makey_bot_iroman.py
+++ makey_bot_iroman.py
@@ -1,31 +1,37 @@
 from gpiozero import LED
+from gpiozero import PWMLED
+import random
 import time
 
 red_led = LED(3)
 yellow_led = LED(15)
 green_led = LED(27)
+r_light_L = PWMLED(25)
+g_light_L = PWMLED(8)
+b_light_L = PWMLED(7)
+r_light_R = PWMLED(5)
+g_light_R = PWMLED(6)
+b_light_R = PWMLED(13)
 
-def stop_light(_colors_dict):
-    for key in _colors_dict:
-        led_str = key + '_led.' + _colors_dict[key] + '()'
-        print(led_str)
+def stop_light(_stop_colors_dict):
+    for key in _stop_colors_dict:
+        led_str = key + '_led.' + _stop_colors_dict[key] + '()'
         eval(led_str)
 
-def dict_change(_colors_dict):
-    for key in _colors_dict:
-        _colors_dict[key] = 'off'
-        print(_colors_dict)
-    return _colors_dict
+def stop_dict_change(_stop_colors_dict):
+    for key in _stop_colors_dict:
+        _stop_colors_dict[key] = 'off'
+    return _stop_colors_dict
 
 def main():
-    colors_dict = {'red' : 'off', 'yellow' : 'off', 'green' : 'off'}
+    stop_colors_dict = {'red' : 'off', 'yellow' : 'off', 'green' : 'off'}
     while True:
         print("Input 'red' 'yellow' or 'green'")
-        user_color = input(">>> ")
-        if(user_color in colors_dict):
-            colors_dict = dict_change(colors_dict)
-            colors_dict[user_color] = 'on'
-            stop_light(colors_dict)
+        user_stop_color = input(">>> ")
+        if(user_stop_color in stop_colors_dict):
+            stop_colors_dict = stop_dict_change(stop_colors_dict)
+            stop_colors_dict[user_stop_color] = 'on'
+            stop_light(stop_colors_dict)
         else:
             print("not a valid color")
 
@@ -41,4 +47,4 @@ main()
 # Inputs: console
 # Outputs: 3x LEDs
 # Created: 03/12/25
-# Updated: 03/14/25
+# Updated: 03/17/25
