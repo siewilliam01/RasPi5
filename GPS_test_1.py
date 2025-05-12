079e99f9140f7c7ee1a1475ab3f16f621b15ee43
siewilliam01 <siewilliam01@gmail.com>
2025-05-12 09:45:52 -0700

    Update GPS_test_1.py

--- GPS_test_1.py
+++ GPS_test_1.py
@@ -1,15 +1,24 @@
-#!/usr/bin/env python3
-# Original Code: https://gist.github.com/Lauszus/5785023#file-gps-py
-# Created by: Kristian Sloth Lauszus
-
 import time
 import serial
 
+data_dict = {}
+
 count = 0
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # Open Serial port
+ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)  # Open Serial port
 while True:
-    line = ser.readline().decode("utf-8")
-    lines = line.split(",")
-    count += 1
-    print(count, lines)
+    raw_line = ser.readline().decode("utf-8")
+    if "GPGGA" in raw_line:
+        count += 1
+        line = raw_line
+        lines = line.split(",")
+        data_dict["time"] = lines[1]
+        data_dict["latitude"] = lines[2]
+        data_dict["N/S"] = lines[3]
+        data_dict["longitude"] = lines[4]
+        data_dict["E/W"] = lines[5]
+        data_dict["quality"] = lines[6]
+        data_dict["satellites"] = lines[7]
+        print(count)
+        for i in data_dict:
+            print(i, ":", data_dict[i])
