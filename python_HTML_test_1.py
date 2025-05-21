549f1737d631e6b5a1ef28ae04534e001320fea6
siewilliam01 <siewilliam01@gmail.com>
2025-05-21 10:18:36 -0700

    Add files via upload

---
+++ python_HTML_test_1.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+from gpiozero import LED
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+host_name = '10.178.202.114'  # IP Address of Raspberry Pi
+host_port = 8000
+
+led = LED(18)
+
+
+def getTemperature():
+    temp = os.popen("vcgencmd measure_temp").read()
+    return temp
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _redirect(self, path):
+        self.send_response(303)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Location', path)
+        self.end_headers()
+
+    def do_GET(self):
+        html = '''
+           <html>
+           <body 
+            style="width:960px; margin: 20px auto;">
+           <h1>Welcome to my Raspberry Pi</h1>
+           <p>Current GPU temperature is {}</p>
+           <form action="/" method="POST">
+               Turn LED :
+               <input type="submit" name="submit" value="On">
+               <input type="submit" name="submit" value="Off">
+           </form>
+           </body>
+           </html>
+        '''
+        temp = getTemperature()
+        self.do_HEAD()
+        self.wfile.write(html.format(temp[5:]).encode("utf-8"))
+
+    def do_POST(self):
+
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode("utf-8")
+        post_data = post_data.split("=")[1]
+
+        
+        if post_data == 'On':
+            led.on()
+        elif post_data == 'Off':
+            led.off()
+
+        print("LED is {}".format(post_data))
+        self._redirect('/')  # Redirect back to the root url
+
+
+# # # # # Main # # # # #
+
+if __name__ == '__main__':
+    http_server = HTTPServer((host_name, host_port), MyServer)
+    print("Server Starts - %s:%s" % (host_name, host_port))
+
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        http_server.server_close()
